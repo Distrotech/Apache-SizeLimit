270ea97876f280dd9df1473792777d423c0683e7
pgollucci <pgollucci@13f79535-47bb-0310-9956-ffa450edef68>
2006-07-07 20:01:47 +0000

    regen with pod2text and linewrap to a suitable length
    git-svn-id: https://svn.apache.org/repos/asf/perl/Apache-SizeLimit/trunk@419968 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Apache/SizeLimit.pm
+++ lib/Apache/SizeLimit.pm
@@ -357,15 +357,17 @@ Apache::SizeLimit - Because size does matter.
 
 =head1 DESCRIPTION
 
-******************************** NOIICE ************************************
-    This version is only for httpd 1.x and mod_perl 1.x series.
+******************************** NOIICE *******************
+
+    This version is only for httpd 1.x and mod_perl 1.x 
+    series.
 
     Future versions of this module may support both.
 
-    Currently, Apache2::SizeLimit is bundled with mod_perl 2.x for that 
-    series.
+    Currently, Apache2::SizeLimit is bundled with 
+    mod_perl 2.x for that series.
     
-******************************** NOTICE ************************************
+******************************** NOTICE *******************
 
 This module allows you to kill off Apache httpd processes if they grow
 too large. You can make the decision to kill a process based on its
